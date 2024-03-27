adef1f2302e22ba16469ac14db861d6e1be59580
Zaria <91369177+zariafk@users.noreply.github.com>
2024-03-27 14:28:00 +0000

    Functioning

--- CaeserCipher.py
+++ CaeserCipher.py
@@ -1,7 +1,7 @@
 # CAESER CIPHER
 import tkinter as tk
 
-# Root window creation + details
+# Root/initial window creation + details
 window = tk.Tk()
 window.geometry ('600x500')
 window.title ('Caeser Cipher by Zaria')
@@ -13,13 +13,61 @@ def clearWindow(window) :
 
 # Adding a label to root window
 label1 = tk.Label(window, text = 'Would you like to encrypt or decrypt text? ')
-label1.grid(column = 0, row = 0)
+label1.grid(column = 0, row = 0, padx = 5, pady = 5)
 
-#Functions for relevant buttons
+# Functions for relevant buttons
+# Global alphabet variable for functions
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# Function to encrypt plaintext
+def encrypt(plain) :
+    key = 3 # typical caeser cipher key
+    cipher = ''
+    for letter in plain :
+        letter = letter.lower()
+        i = alphabet.find(letter)
+        if i == -1 :
+            cipher += letter
+        else : # accounts for x,y,z where there are <3 letters after them
+            ci = i+key
+            if ci > 25 :
+                ci -= 26
+            cipher += alphabet[ci]
+    return cipher
+
+# Function to decrypt ciphertext
+def decrypt(cipher) :
+    key = 3 # typical caeser cipher key
+    plain = ''
+    for letter in cipher :
+        letter = letter.lower()
+        ci = alphabet.find(letter)
+        if ci == -1 :
+            plain += letter
+        else : # accoutns for a,b,c where there are <3 letters before them
+            i = ci-key
+            if i < 0 :
+                i += 26
+            plain += alphabet[i]
+    return plain
+
+# When encrypt button is pressed at encryption window
+def eExecClick(plain) :
+    cipher = encrypt(plain)
+    cipherLabel = tk.Label(window, text = cipher)
+    cipherLabel.grid(column = 0, row = 4, pady = 30)
+
+# When decrypt button is pressed at decryption window
+def dExecClick(cipher) :
+    plain = decrypt(cipher)
+    plainLabel = tk.Label(window, text = plain)
+    plainLabel.grid(column = 0, row = 4, pady = 30)
+
+# When the encryption button is pressed from initial window
 def eClick() :
     clearWindow(window)
     eLabel = tk.Label(window, text = 'Encryption Mode')
-    eLabel.grid(column = 0, row = 0, padx = 5, sticky = 'w')
+    eLabel.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = 'w')
     
     eInLabel = tk.Label(window, text = 'Please enter the text you would like to encrypt: ')
     eInLabel.grid(column = 0, row = 1, padx = 5, pady = 20, sticky = 'w')
@@ -27,13 +75,14 @@ def eClick() :
     eInput = tk.Entry(window, width = 75)
     eInput.grid(column = 0, row = 2, padx = 70, pady = 20, sticky = 'sw')
 
-    eExec = tk.Button(window, text = 'Encrypt', fg = 'red')
+    eExec = tk.Button(window, text = 'Encrypt', fg = 'red', command = lambda: eExecClick(eInput.get()))
     eExec.grid(column = 0, row = 3, padx = 280, pady = 5, sticky = 'sw')
 
+# When decryption button is pressed from initial window
 def dClick() :
     clearWindow(window)
     dLabel = tk.Label(window, text = 'Decryption Mode')
-    dLabel.grid(column = 0, row = 0, padx = 5, sticky = 'w')
+    dLabel.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = 'w')
 
     dInLabel = tk.Label(window, text = 'Please enter the text you would like to decrypt: ')
     dInLabel.grid(column = 0, row = 1, padx = 5, pady = 20, sticky = 'w')
@@ -41,10 +90,10 @@ def dClick() :
     dInput = tk.Entry(window, width = 75)
     dInput.grid(column = 0, row = 2, padx = 70, pady = 20, sticky = 'sw')
 
-    dExec = tk.Button(window, text = 'Decrypt', fg = 'Green')
+    dExec = tk.Button(window, text = 'Decrypt', fg = 'Green', command = lambda: dExecClick(dInput.get()))
     dExec.grid(column = 0, row = 3, padx = 280, pady = 5, sticky = 'sw')
 
-# Creating relevant buttons
+# Creating buttons for initial window
 eButton = tk.Button(window, text = 'Encrypt', bg = 'red', command = eClick)
 eButton.grid(column = 1, row = 1, padx = 10, pady = 50)
 dButton = tk.Button(window, text = 'Decrypt', bg = 'green', command = dClick)
@@ -52,52 +101,3 @@ dButton.grid(column = 2, row = 1, padx = 10, pady = 50)
 
 # Execute tkinter
 window.mainloop()
-
-'''
-def encrypt(text, key) :
-    cipher = ''
-    for letter in text :
-        letter = letter.lower()
-        i = alphabet.find(letter)
-        if i == -1 :
-            cipher += letter
-        else :
-            ci = i+3
-            if ci > 25 :
-                ci -= 26
-            cipher += alphabet[ci]
-    return cipher
-
-def decrypt(cipher, key) :
-    text = ''
-    for letter in cipher :
-        letter = letter.lower()
-        ci = alphabet.find(letter)
-        if ci == -1 :
-            text += letter
-        else :
-            i = ci-3
-            if i < 0 :
-                i += 26
-            text += alphabet[i]
-    return text
-
-def main() :
-    global alphabet
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    key = 3
-
-    selection = input('Do you want to encrypt or decrypt?\n[Please enter e/d]\n')
-    
-    if selection != 'e' and selection != 'd' :
-        print('Invalid input')
-    else :
-        text = input('Please enter text: ')
-        if selection == 'e' :
-            print(encrypt(text, key))
-        elif selection == 'd' :
-            print(decrypt(text, key)) 
-    return
-
-main()
-'''
