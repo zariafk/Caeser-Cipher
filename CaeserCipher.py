292acd021a9114d0d0dfc17f85bb169601f9869d
Zaria <91369177+zariafk@users.noreply.github.com>
2024-03-29 20:10:38 +0000

    Efficiency Improvals

--- CaeserCipher.py
+++ CaeserCipher.py
@@ -1,7 +1,7 @@
-# CAESER CIPHER
+# CAESER CIPHER ~by Zaria Farooq-Khan
 import tkinter as tk
 
-# Initial functions
+                                                                        # BASIC FUNCTIONS
 # To clear window 
 def clearWindow(window) :
     for widget in window.winfo_children() :
@@ -12,7 +12,6 @@ def clearWindow(window) :
 def homeButton() :
     hButton = tk.Button(window, text = 'Home', bg = 'black', fg = 'white', command = lambda: startRender())
     hButton.grid(column = 0, row = 0, padx = 20, pady = 5, sticky = 'e')
-
 
 
                                                                     # ENCRYPT/DECRYPT FUNCTION
@@ -39,57 +38,42 @@ def eOrD(text, ed) :
     return result
 
 
-
                                                                         # BUTTON FUNCTIONS
-# When encrypt button is pressed at encryption window   
-def eExecClick(plain) :
-    cipher = eOrD(plain, 'e')
-    cipherLabel = tk.Label(window, text = cipher)
-    cipherLabel.grid(column = 0, row = 4, pady = 30)
+# When encrypt/decrypt button is pressed at encryption/decryption window, respectively
+def calcClick(text, ed) :
+    result = eOrD(text, ed)
+    resultLabel = tk.Label(window, text = result)
+    resultLabel.grid(column = 0, row = 4, pady = 30)
 
 
-# When decrypt button is pressed at decryption window
-def dExecClick(cipher) :
-    plain = eOrD(cipher, 'd')
-    plainLabel = tk.Label(window, text = plain)
-    plainLabel.grid(column = 0, row = 4, pady = 30)
-
-
-# When the encryption button is pressed from initial window
-def eClick() :
+# New rendering once encryption/decryption mode is selected (from initial window)
+def modes(ed) :
     clearWindow(window)
     homeButton()
 
-    eLabel = tk.Label(window, text = 'Encryption Mode')
-    eLabel.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = 'w')
-    
-    eInLabel = tk.Label(window, text = 'Please enter the text you would like to encrypt: ')
-    eInLabel.grid(column = 0, row = 1, padx = 5, pady = 20, sticky = 'w')
+    # Only 'e' or 'd' will ever be passed in as ed, for encryption and decryption, respectively
+    if ed == 'e' :
+        labelText = 'Encryption'
+        inpLabelText = 'encrypt'
+        calcText = 'Encrypt'
+        colr = 'red'
+    else :
+        labelText = 'Decryption'
+        inpLabelText = 'decrypt'
+        calcText = 'Decrypt'
+        colr = 'green'
 
-    eInput = tk.Entry(window, width = 75)
-    eInput.grid(column = 0, row = 2, padx = 70, pady = 20, sticky = 'sw')
+    label = tk.Label(window, text = labelText + 'Mode')
+    label.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = 'w')
 
-    eExec = tk.Button(window, text = 'Encrypt', fg = 'red', command = lambda: eExecClick(eInput.get()))
-    eExec.grid(column = 0, row = 3, padx = 280, pady = 5, sticky = 'sw')
+    inpLabel = tk.Label(window, text = 'Please enter the text you would like to ' + inpLabelText + ': ')
+    inpLabel.grid(column = 0, row = 1, padx = 5, pady = 5, sticky = 'w')
 
+    inp = tk.Entry(window, width = 75)
+    inp.grid(column = 0, row = 2, padx = 70, pady = 20, sticky = 'sw')
 
-# When decryption button is pressed from initial window
-def dClick() :
-    clearWindow(window)
-    homeButton()
-
-    dLabel = tk.Label(window, text = 'Decryption Mode')
-    dLabel.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = 'w')
-
-    dInLabel = tk.Label(window, text = 'Please enter the text you would like to decrypt: ')
-    dInLabel.grid(column = 0, row = 1, padx = 5, pady = 20, sticky = 'w')
-
-    dInput = tk.Entry(window, width = 75)
-    dInput.grid(column = 0, row = 2, padx = 70, pady = 20, sticky = 'sw')
-
-    dExec = tk.Button(window, text = 'Decrypt', fg = 'Green', command = lambda: dExecClick(dInput.get()))
-    dExec.grid(column = 0, row = 3, padx = 280, pady = 5, sticky = 'sw')
-
+    calc = tk.Button(window, text = calcText, fg = colr, command = lambda: calcClick(inp.get(), ed))
+    calc.grid(column = 0, row = 3, padx = 280, pady = 5, sticky = 'sw')
 
 
                                                                                 # START WINDOW
@@ -108,17 +92,18 @@ def startRender() :
     label1.grid(column = 0, row = 0, padx = 5, pady = 5)
 
     # Creating buttons for initial window
-    eButton = tk.Button(window, text = 'Encrypt', bg = 'red', command = eClick)
+    eButton = tk.Button(window, text = 'Encrypt', bg = 'red', command = lambda: modes('e'))
     eButton.grid(column = 1, row = 1, padx = 10, pady = 50)
-    dButton = tk.Button(window, text = 'Decrypt', bg = 'green', command = dClick)
+    dButton = tk.Button(window, text = 'Decrypt', bg = 'green', command = lambda: modes('d'))
     dButton.grid(column = 2, row = 1, padx = 10, pady = 50)
 
 
-
-# Execute
+                                                    # WINDOW RENDERING AND EXECUTION - FINAL
+# Window rendering
 window = tk.Tk()
 window.geometry ('600x500')
 window.title ('Caeser Cipher by Zaria')
 
+# Execution
 startRender()
 window.mainloop()
